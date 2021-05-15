fd1296f5b7ee22298bebaf1d9b4dac60858097f9
Sim0nV <simon.valenzona@yahoo.com>
2021-05-15 14:32:30 -0700

    Basic file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+//Importing Components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <header>
+        <h1>Ed's Todo List</h1>
+      </header>
+      <Form />
+      <TodoList />
     </div>
   );
 }
