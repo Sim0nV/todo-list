6fb02d78a7a6172d2eaf2d5f9c5cfb7efde17506
Sim0nV <simon.valenzona@yahoo.com>
2021-05-15 17:47:16 -0700

    new completeHandler function: completed toggle

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,10 +5,22 @@ const Todo = ({ text, todo, todos, setTodos }) => {
     const deleteHandler = () => {
         setTodos(todos.filter((el) => el.id !== todo.id));
     };
+    const completeHandler = () => {
+        setTodos(todos.map((item) => {
+            if(item.id === todo.id) {
+                return { //return props of whatever id + toggle completed
+                    ...item, 
+                    completed: !item.completed
+                };
+            }
+            return item; //If no matches, just return item w/ no changes
+        })
+        );
+    };
     return(
         <div className="todo">
             <li className='todo-item'>{text}</li>
-            <button className="complete-btn">
+            <button onClick={completeHandler} className="complete-btn">
                 <i className="fas fa-check"></i>
             </button>
             <button onClick={deleteHandler} className="trash-btn">
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,8 +4,8 @@ import Todo from './Todo';
 const TodoList = ({ todos, setTodos }) => {
     return(
         <div className="todo-container">
-            <ul className="todo-list">
-                {todos.map((todo) => (
+            <ul className="todo-list"> 
+                {todos.map((todo) => ( //Map: Cycles through each element of array (todos array)
                     <Todo 
                         setTodos={setTodos} 
                         todos={todos} 
