8ca5ec23fe03cd143968b4fb28546c9d07d631a7
Sim0nV <simon.valenzona@yahoo.com>
2021-05-16 15:34:26 -0700

    Add filtering for todo list
    Add status state for current filter status.
    Add filteredTodos array state to hold filtered todos
    based on the status state.
    Output filteredTodos array instead of todos array in
    TodoList function so filtered todo list is outputted.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 //Importing Components
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
+//DEBUG: Add function headers?
+
 function App() {
+  //State Stuff
   //[actual value, function to change value]
   const [inputText, setInputText] = useState("");
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]); //DEBUG rename to todoArray?
+  const [status, setStatus] = useState('all'); //DEBUG rename to filterStatus?
+  const [filteredTodos, setFilteredTodos] = useState([])
+
+  //USE EFFECT
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+
+  //Functions
+  const filterHandler = () => {
+    switch(status) {
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;    
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
+
   return (
     <div className="App">
       <header>
         <h1>Ed's Todo List</h1>
       </header>
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
-      <TodoList setTodos={setTodos} todos={todos} />
+      <Form 
+        inputText={inputText} 
+        todos={todos} 
+        setTodos={setTodos} 
+        setInputText={setInputText} 
+        setStatus = {setStatus} 
+      
+      />
+      <TodoList 
+        filteredTodos={filteredTodos} 
+        setTodos={setTodos} 
+        todos={todos} 
+      />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 //use FUNCTIONAL components later
-const Form = ({ setInputText, todos, setTodos, inputText }) => {
+const Form = ({ setInputText, todos, setTodos, inputText, setStatus}) => {
     //write javascript code and function
     const inputTextHandler = (e) => {    
         console.log(e.target.value); //debug print
@@ -18,7 +18,10 @@ const Form = ({ setInputText, todos, setTodos, inputText }) => {
             ...todos, {text: inputText, completed: false, id: Math.random() * 1000}
         ]);
         setInputText(''); //Clear input text once todo submitted
-    }
+    };
+    const statusHandler = (e) => {
+        setStatus(e.target.value);
+    };
     return(
         <form>
         <input value ={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
@@ -26,7 +29,7 @@ const Form = ({ setInputText, todos, setTodos, inputText }) => {
             <i className="fas fa-plus-square"></i>
         </button>
         <div className="select">
-            <select name="todos" className="filter-todo">
+            <select onChange={statusHandler} name="todos" className="filter-todo">
             <option value="all">All</option>
             <option value="completed">Completed</option>
             <option value="uncompleted">Uncompleted</option>
