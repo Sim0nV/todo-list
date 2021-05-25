3606ea094053e6661c3bc9858abdea51c426ccd8
Sim0nV <simon.valenzona@yahoo.com>
2021-05-24 20:04:22 -0700

    Split reducer functions
    Add new components folder under src.
    Relocate reducer functions to their own files under /reducers/
    Import reducer functions to App.js

---
+++ src/reducers/filterStatus.js
@@ -0,0 +1,29 @@
+/*
+ * filterStatus: Filter status reducer
+ * parameters: filter status state, action
+ * return value: new filter status state based on 
+ * passed action's type and filter status
+ */
+export const filterStatus = (state = 'all', action) => {
+
+    switch(action.type) {
+  
+      case 'SET_FILTER_STATUS':
+  
+        switch(action.filterStatus) {
+  
+          case 'all':
+          case 'completed':
+          case 'uncompleted':
+            return action.filterStatus;
+  
+          default:
+            return state;
+        }
+        
+      default:
+        return state;
+  
+    }
+  
+}
---
+++ src/reducers/inputText.js
@@ -0,0 +1,19 @@
+/*
+ * inputText: Input text reducer
+ * parameters: input text state, action
+ * return value: new input text state based on 
+ * passed action's type and input text
+ */
+export const inputText = (state = "", action) => {
+
+    switch(action.type) {
+      
+      case 'SET_INPUT_TEXT':
+        return action.inputText;
+  
+      default:
+        return state; 
+  
+    }
+
+} 
