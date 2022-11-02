188ffaf75c5b62c9114445e0bee03962f25bcaf0
SimonParadigm <simon.valenzona@paradigmiot.com>
2022-11-01 19:00:37 -0700

    Add local storage for todos

--- src/stores/useBoundStore.js
+++ src/stores/useBoundStore.js
@@ -1,8 +1,18 @@
 import create from "zustand";
+import { persist } from "zustand/middleware";
 import { createTodoSlice } from "./todoSlice";
 import { createFilterSlice } from "./filterSlice";
 
-export const useBoundStore = create((...set) => ({
-  ...createTodoSlice(...set),
-  ...createFilterSlice(...set)
-}));
+export const useBoundStore = create(
+  persist(
+    (...set) => ({
+      ...createTodoSlice(...set),
+      ...createFilterSlice(...set)
+    }),
+    {
+      name: "todosStore",
+      getStorage: () => localStorage,
+      partialize: (state) => ({ todosArray: state.todosArray })
+    }
+  )
+);
