cb4e98e21c24578c5d6cfbc6c4958fef7bef32b1
SimonParadigm <simon.valenzona@paradigmiot.com>
2022-11-01 18:35:15 -0700

    Add toggleCompleteTodo function to store

--- src/stores/useStore.js
+++ src/stores/useStore.js
@@ -8,8 +8,19 @@ export const useStore = create((set) => ({
     set(() => ({ filterStatus: filterStatus })),
   addTodo: (todo) =>
     set((state) => ({ todosArray: state.todosArray.concat(todo) })),
-  deleteTodo: (todo) => 
+  deleteTodo: (todo) =>
     set((state) => ({
       todosArray: state.todosArray.filter((currTodo) => currTodo !== todo),
     })),
+  toggleCompleteTodo: (todo) =>
+    set((state) => {
+      let newTodos = state.todosArray.slice();
+      newTodos.splice(newTodos.indexOf(todo), 1, {
+        ...todo,
+        completed: !todo.completed,
+      });
+      return {
+        todosArray: newTodos,
+      };
+    })
 }));
