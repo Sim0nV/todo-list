5a78aae2d5a291f51f2b78f5c78502fbb797adf0
SimonParadigm <simon.valenzona@paradigmiot.com>
2022-11-01 17:27:40 -0700

    Add addTodo function to store

--- src/stores/useStore.js
+++ src/stores/useStore.js
@@ -1,9 +1,9 @@
 import create from "zustand";
 
-
 export const useStore = create((set) => ({
     todosArray: [],
     filterStatus: "all",
     setTodosArray: (todosArray) => set(() => ({todosArray: todosArray})),
-    setFilterStatus: (filterStatus) => set(() => ({filterStatus: filterStatus}))
+    setFilterStatus: (filterStatus) => set(() => ({filterStatus: filterStatus})),
+    addTodo: (todo) => set((state) => ({todosArray: state.todosArray.concat(todo)}))
 }));
